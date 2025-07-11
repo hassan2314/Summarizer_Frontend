296fdf1e85785e1f280e42e9d5ce5cad8b302a6c
Hassan Ahmed <hassan87691@gmail.com>
2025-07-11 17:30:46 +0500

    Signup toLogin

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -43,8 +43,7 @@ const Signup = () => {
         }
       );
       if (res) {
-        dispatch(loginSuccess({ user: res.data.user, token: res.data.token }));
-        navigate("/");
+        navigate("/login");
       }
     } catch (err) {
       const msg = err.response?.data?.message || "Signup failed";
